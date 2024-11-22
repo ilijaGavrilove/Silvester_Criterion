cbef6e4e1fbefc4af17a622b3945a11cb26411f9
ilijaGavrilove <157256405+ilijaGavrilove@users.noreply.github.com>
2024-11-22 19:49:01 +0300

    Update SquareMatrix.java

--- SquareMatrix.java
+++ SquareMatrix.java
@@ -1,10 +1,24 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class SquareMatrix {
 
-    private static int size = 0;
+    private int size = 0;
+    private final double[][] matrix;
 
-    private static double calcDeterminant(double[][] matrix){
+
+    public SquareMatrix getMinor(int row, int col) {
+        double[][] resultMinor = new double[size - 1][size - 1];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i != row && j != col) {
+                    resultMinor[i < row ? i : i - 1][j < col ? j : j - 1] = matrix[i][j];
+                }
+            }
+        }
+        return new SquareMatrix(size - 1, resultMinor);
+    }
+
+    public double calcDeterminant(){
         double det = 0;
         // Базовый случай для 1x1 матрицы
         if (size == 1) {
@@ -17,46 +31,67 @@ public class SquareMatrix {
         }
 
         for (int i = 0; i < size; i++) {
-            det += Math.pow(-1, i) * matrix[0][i] * calcDeterminant(minor(matrix, 0, i));
+            det += Math.pow(-1, i) * matrix[0][i] * getMinor(0, i).calcDeterminant();
         }
 
         return det;
     }
-
-    private static double[][] minor(double[][] matrix, int row, int col) {
-        double[][] minor = new double[size - 1][size - 1];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (i != row && j != col) {
-                    minor[i < row ? i : i - 1][j < col ? j : j - 1] = matrix[i][j];
-                }
+    public SquareMatrix getMainMinor(int degree) {
+        double[][] resultMatrix = new double[degree][degree];
+        for ( int i = 0 ; i < degree ; i++ ) {
+            for ( int j = 0 ; j < degree ; j++ ) {
+                resultMatrix[i][j] = matrix[i][j];
             }
         }
-        return minor;
+        return new SquareMatrix(degree, resultMatrix);
     }
 
-    public SquareMatrix(int size){this.size = size;}
+    public SquareMatrix(int size, double[][] matrix) {
+        this.size = size;
+        this.matrix = matrix;
+    }
 
     public int getSize() {return size;}
 
-    public double[][] create(){
-        double[][] result = new double[size][size];
-        Scanner scanner = new Scanner(System.in);
+    public double[][] getMatrix() {return matrix;}
 
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for ( int i = 0 ; i < size ; i++) {
+            builder.append(Arrays.toString(matrix[i]));
+            builder.append("\n");
+        }
+        return builder.toString();
+    }
+
+    public FormTypes defineForm(){
+        int[] mainMinorSigns = new int[size];
+        int tmp = -1;
         for(int i = 0; i < size; ++i){
-            for(int j = i; j < size; ++j){
-                System.out.println("Введите элемент матрицы " + Integer.toString(i+1) + ' ' + Integer.toString(j+1) + ":");
-                result[i][j] = scanner.nextDouble();
-                result[j][i] = result[i][j];
+            if(getMainMinor(i+1).calcDeterminant() != 0) {
+                mainMinorSigns[i] = getMainMinor(i + 1).calcDeterminant() > 0 ? 1 : -1;
+            } else {
+                return FormTypes.NONE;
             }
         }
-        return result;
-    }
 
-    public static FormTypes defineForm(double[][] matrix){
-        //for(int i = 0; i < size; ++i){
 
-        //}
-        return FormTypes.NONE;
+        if(mainMinorSigns[0] == 1){
+            for(int i = 0; i < mainMinorSigns.length; ++i){
+                if(mainMinorSigns[i] == -1){return FormTypes.NONE;}
+            }
+            return FormTypes.POSITIVE;
+        }
+
+        else{
+            for(int i = 0; i < mainMinorSigns.length; ++i){
+                tmp *= (int) Math.pow(-1, i);
+                if(tmp != mainMinorSigns[i]){return FormTypes.NONE;}
+            }
+            return FormTypes.NEGATIVE;
+        }
     }
 }
+
+
